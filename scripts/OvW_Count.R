c474ef015d173be49bac8067cda5993d844ed1e7
eprileson <egp43@case.edu>
2022-10-31 17:23:29 -0400

    finalize proportional model and results

--- scripts/OvW_Count.R
+++ scripts/OvW_Count.R
@@ -27,14 +27,11 @@ count$Worker.loss <- (count$Worker.start - count$Worker.end) #make worker loss p
 head(count)
 
 #create new column of worker number change as a proportion
-count$worker.prop <- ((count$Worker.loss)/(count$Worker.start))
+count$worker.prop <- ((count$Worker.end)/(count$Worker.start))
 
-#replace 1s in prop data with 0.999
-count$worker.prop[count$worker.prop==1] <- 0.9999999
-head(count)
 ###Part 3: Exploratory Graphics: initial look at data
 plot(Worker.loss ~ Source.pop, data = count) #change in worker numb. ~ source pop
-plot(Worker.loss ~ Collection_date, data = count) #no relationship it seems
+plot(worker.prop ~ Source.pop, data = count) #no relationship it seems
 plot(Collection_date ~ Source.pop, data = count) #rural collected at start and end; urban in middle period
 
 #shows urban pops have had bigger change in survival than rural
@@ -56,6 +53,7 @@ class(count1$Collection_date) #changed to date that R recognizes
 
 #change name to col season
 colnames(count1) <- c("Colony_ID", "Source.pop", "Beginning", "End", "Brood_count", "Col_Season")
+head(count1)
 
 #pivot the data longer so that we can show the change over time with the data set
 library(magrittr)
@@ -65,18 +63,20 @@ count1 <- count1 %>%
 
 count1$Time <- as.factor(count1$Time)
 class(count1$Time)
-head(count1)
+
 
 #exploratory graphics: (worker pop ~ time*source.pop)
 library(ggplot2)
-ggplot(count, aes(x = Source.pop, y = worker.prop, colour = Source.pop))+
+ggplot(count1, aes(x = Time, y = Worker.pop, colour = Source.pop))+
   geom_errorbar(stat = 'summary', fun.data = 'mean_se', 
                 width=0.05, fun.args = list(mult = 1))+
-  geom_point(size = 7, stat = 'summary', fun.y = "mean")+
+  geom_point(aes(colour = factor(Source.pop)), size = 7, stat = 'summary', fun.y = "mean")+
+  geom_segment(aes(x = 1, y = 45.5, xend = 2, yend = 13), color = "cadetblue")+
+  geom_segment(aes(x = 1, y = 62, xend = 2, yend = 15), color = "darkorange")+
   scale_colour_manual(values = c("cadetblue", "darkorange"))+
   theme_classic()+
-  labs(y = "Proportion of Worker Loss", x = "Source Population")+
-  ggtitle('Proportion Worker Change')+
+  labs(y = "Worker Population", x = "Census Points", color = "Population")+
+  ggtitle('Colony Population Change')+
   theme(
     title = element_text(size = 14),
     axis.title = element_text(size = 14),
@@ -111,8 +111,7 @@ mean(count[c(10:30, 43:44),4]) #urban mean = 14.04
 ##Part 4: Model development
 library(glmmTMB)
 library(nlme)
-library(lme4)
-library(MASS)
+
 #first model for worker loss over 4 mo
 #FINAL MODEL 1 - 
 mod.control2 <- glmmTMBControl(optimizer=optim,
@@ -122,90 +121,78 @@ surv.mod <- glmmTMB(Worker.loss ~ Source.pop + (1 | ColonyID) + (1 | Collection_
                     family = poisson(link = "log"))
 
 #FINAL MODEL 2: Proportional Worker Loss
-#adjust prop data = 1 to be 0.999
-prop.surv <- glmmTMB(worker.prop ~ Source.pop  + (1 | Col_Season),
-                    data=count, family = beta_family(link = "logit"),
-                    weights = Worker.start)
+prop.surv <- glmmTMB(worker.prop ~ Source.pop + (1 | Colony_ID) + (1 | Col_Season),
+                    data=count)
+#model w/ quasi binomial - doesn't work with glmmTMB
+library(lme4)
+library(MASS)
 
-#model with binomial dist to try
-prop.surv2 <- glmmTMB(worker.prop ~ Source.pop + (1 |Col_Season),
-                      weights= Worker.start, data=count, family = binomial(link = "logit"))
+prop.surv2 <- betareg(worker.prop ~ Source.pop, random =  ~1 |Col_Season,
+                      weights= Worker.start, link = 'log', data=count)
+#model with lme built in?
+prop.surv3 <- glmmTMB(worker.prop ~ Source.pop, random = ~ 1 | Col_Season,
+                      weights= Worker.start, family= quasibinomial(link = 'log'), data=count)
 
-#model with quasi-binomial using glm
-prop.surv3 <- glm(worker.prop ~ Source.pop*Col_Season,
-                  data=count, family = quasibinomial(link = "logit"),
-                  weights = Worker.start)
-
-#FINAL MODEL: Change model (w/ both end and beg)
-change.mod <- glmmTMB(Worker.pop ~ Time*Source.pop + (1 | Col_Season),
+#FINAL MODEL 1: Change model (w/ both end and beg)
+change.mod <- glmmTMB(Worker.pop ~ Time*Source.pop + (1 | Colony_ID) + (1 | Col_Season),
                       data = count1, family = poisson(link = "log"))
 
+#FINAL MODEL 2: Proportion of worker ants remaining after winter
+prop.surv1 <- glmmTMB(worker.prop ~ Source.pop + (1 | Col_Season),
+                      weights= Worker.start, family = betabinomial(link = "logit"), data=count)
 
-## Part 5: Model diagnostics
-#QQplots, residuals, AIC
-##Diagnostics: warning msgs for models - show model convergence problem w/ small eigen value problems
-diagnose(change.mod) #removed the interaction effect due to extremely large SD
-
-diagnose(prop.surv3) 
-
-library(DHARMa) #use simulated diagnostic modeling since we have a glmm; works with both glmer and glmmTMB objects
-simOutput3 <- simulateResiduals(fittedModel = prop.surv3)
-plot(simOutput3)
-plotResiduals(simOutput3)
-testOutliers(simOutput3, margin = c("both"), type = c("bootstrap"), plot = T) #outliers are not signf. here
-
-#regular diagnostic tests for glm
-plot(prop.surv3)
 
 #plot basic modeled relationship to see if it matches expl graphics
 library(visreg)
 visreg(prop.surv)
 
+## Part 5: Model diagnostics
+#QQplots, residuals, AIC
+##Diagnostics: warning msgs for models - show model convergence problem w/ small eigen value problems
+diagnose(end.mod) #removed the interaction effect due to extremely large SD
+
+diagnose(prop.surv1)  #really good fit both w/ DHARMa and diagnose()
+
+library(DHARMa) #use simulated diagnostic modeling since we have a glmm; works with both glmer and glmmTMB objects
+simOutput3 <- simulateResiduals(fittedModel = prop.surv1, plot = F)
+plot(simOutput3)
+
+testOutliers(simOutput3, margin = c("both"), type = c("bootstrap"), plot = T) #outliers are not signf. here
+
 
 ##Part 6: Statistical Testing
 #gather summary data on model, determine model fit
-summary(surv.mod)
-summary(prop.surv3)
-summary(change.mod) #time = -1.31, urbanpop = 4.177, time*urbanpop = -0.172
+summary(prop.surv1) #urban = -.093 lower proportion workers left SE = 0.19
+summary(change.mod) #time = -1.31, urbanpop = 0.226, time*urbanpop = -0.172
 
 # log lik statistic tests for model significance
 library(car)
-Anova(surv.mod, type = "III") #log likelihood
-Anova(prop.surv3, type = "III")
-Anova(change.mod, type = "III") #source pop = chisq = 0.71, p = 0.399
+Anova(prop.surv1, type = "III") #chisq = 0.236; p = 0.627
+Anova(change.mod, type = "III") #source pop = chisq = 67.34, p = 0.399
 #time = 396.03, p < 2e-16***, sourcepop*time = chisq = 3.64, p = 0.056
 
 #backtransform log-linked data:
 #using emmeans for figure and backtransformed values
 #emmeans
 library(emmeans)
-surv.em <- emmeans(surv.mod, pairwise ~ Source.pop, transform = "response", adjust = "fdr")
-surv.em
 change.em <- emmeans(change.mod, pairwise ~ Time*Source.pop, transform = "response", adjust = "fdr")
 change.em
 ##Part 7: Plotting Modeled Output
 #
 #Make follow up plot of modeled results
 library(ggeffects)
-prop.graph <- ggpredict(prop.surv3, terms = c("Source.pop"), 
-                        type = "fe",ci.lvl = 0.68, width=0.05, colour = my_colors3)
-plot(prop.graph) #basic modeled plot
-
+library(ggplot2)
+prop.graph <- ggpredict(prop.surv1, terms = c("Source.pop"), 
+                        type = "fe", allow.new.levels = TRUE,ci.lvl = 0.68, width=0.05, title = FALSE)
+  
 
 #make emmeans objects into dfs for visualization
 #for survival overall
-surv_df <- surv.em$emmeans %>%
-  confint() %>%
-  as.data.frame()
-#make upper and lower SEs for Surv.em; manually calculated from emmeans
-upper.SES <- c(29.56, 42.51)
-lower.SES <- c(20.44, 29.23)
 
 #for proportions - use summary data since not on Log scale
-
-prop_em <- emmeans(prop.surv, pairwise ~ Source.pop, transform = "response", adjust = "fdr")
-upper.SEp <- c(0.7958, 0.8057)
-lower.SEp <- c(0.732, 0.744)
+prop_em <- emmeans(prop.surv1, pairwise ~ Source.pop, transform = "logit", adjust = "fdr")
+upper.SEp <- c(0.29, 0.27)
+lower.SEp <- c(0.22, 0.21)
 
 prop_df <- prop_em$emmeans %>%
   confint()%>%
@@ -215,10 +202,11 @@ prop_df <- prop_em$emmeans %>%
 change_df <- change.em$emmeans %>%
   confint()%>%
   as.data.frame()
-upper.SEg <- c(48.56, 13.22, 71.43, 16.41)
-lower.SEg <- c(39.84, 10.58, 58.77, 13.19)
+upper.SEg <- c(43.12, 11.7, 54.38, 12.41)
+lower.SEg <- c(28.92, 7.74, 35.94, 8.11)
 
 #plot graphs
+
 library(ggplot2)
 #change
 change <- ggplot(change_df, aes(x = Time, y= rate))+  
@@ -235,32 +223,19 @@ change <- ggplot(change_df, aes(x = Time, y= rate))+
   )
 
 #total lost
-total <- ggplot(surv_df, aes(x = Source.pop, y= rate))+  
-  geom_point(colour = my_colors3, size = 7)+
-  geom_errorbar(aes(ymin = lower.SES, ymax = upper.SES), fun.data = 'mean_se', 
-                width=0.05, colour = my_colors3, fun.args = list(mult = 1))+
-  labs(y="Worker Population Loss", x = "Source Population")+
-  ggtitle('Winter Population Loss')+
-  theme_classic()+
-  theme(
-    title = element_text(size = 10),
-    axis.title = element_text(size = 10),
-    axis.text = element_text(size = 10)
-  )
 
 #workers lost as a proportion of starting size
-prop <- ggplot(prop_df, aes(x = Source.pop, y = response))+  
+prop <- ggplot(prop.graph, aes(x, y = predicted))+
   geom_point(colour = my_colors3, size = 7)+
   geom_errorbar(aes(ymin = lower.SEp, ymax = upper.SEp), fun.data = 'mean_se', 
-                width=0.05, colour = my_colors3, fun.args = list(mult = 1))+
-  labs(y="Proportional Worker Loss", x = "Source Population", tag = "B")+
+                width=0.05, fun.args = list(mult = 1), colour = my_colors3)+
+  labs(y="Proportion of Workers Remaining", x = "Source Population", tag = "B")+
   theme_classic()+
   theme(
     title = element_text(size = 14),
     plot.tag = element_text(size = 16, hjust = 0.5),
-    axis.title = element_text(size = 14),
     axis.text = element_text(size = 10)
-  )
+  ) 
 
 #make graphs into combined facets
 library(cowplot)
